a7080730a7d1c79336bb1150cd3b2f33c5c96696
arginsen <arginsen19@163.com>
2020-04-07 01:06:20 +0800

    Add index page

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 '''
+第一次练习
+
 from flask import Flask
 app = Flask(__name__)
 
@@ -20,6 +22,9 @@ from flask import escape
 def user_back(name):
     return 'User: %s' % escape(name)
 '''
+
+'''
+第二次练习
 
 from flask import url_for, escape
 from flask import Flask
@@ -46,3 +51,26 @@ def test_url_for():
     # 下面这个调用传入了多余的关键字参数，它们会被作为查询字符串附加到 URL 后面。
     print(url_for('test_url_for', num=2))  # 输出：/test?num=2
     return 'Test page'
+'''
+from flask import Flask, render_template
+app = Flask(__name__)
+
+# ...
+
+@app.route('/')
+def index():
+    return render_template('index.html', name=name, movies=movies)
+
+name = 'Arginsen'
+movies = [
+    {'title': 'My Neighbor Totoro', 'year': '1988'},
+    {'title': 'Dead Poets Society', 'year': '1989'},
+    {'title': 'A Perfect World', 'year': '1993'},
+    {'title': 'Leon', 'year': '1994'},
+    {'title': 'Mahjong', 'year': '1996'},
+    {'title': 'Swallowtail Butterfly', 'year': '1996'},
+    {'title': 'King of Comedy', 'year': '1999'},
+    {'title': 'Devils on the Doorstep', 'year': '1999'},
+    {'title': 'WALL-E', 'year': '2008'},
+    {'title': 'The Pork of Music', 'year': '2012'},
+]
